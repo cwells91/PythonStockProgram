2b15cae738bcce1bb162ac11d585d72c09edde0f
cwells <chriswellsfl1991@gmail.com>
2023-06-23 00:42:49 -0400

    ready to test if it'll run on a schedule.

--- ticker_test.py
+++ ticker_test.py
@@ -1,14 +1,39 @@
 import yfinance as yf
+import sys
+import signal
+import schedule
+# import time
 
-msft = yf.Ticker("GOOGL")
+# msft = yf.Ticker("GOOGL")
+#
+# # get all stock info
+# print(msft.info)
 
-# get all stock info
-print(msft.info)
+
+def signal_handler(signal, frame):
+    print("\nClosing")
+    sys.exit(0)
 
 
-tickers = ['ABEV3.SA']
-for ticker in tickers:
-    ticker_yahoo = yf.Ticker(ticker)
-    data = ticker_yahoo.history()
-    last_quote = data['Close'].iloc[-1]
-    print(ticker, last_quote)
+tickers = ['GOOGL']
+
+
+# tickers = load data from saved file
+def job():
+    for ticker in tickers:
+        ticker_yahoo = yf.Ticker(ticker)
+        data = ticker_yahoo.history()
+        last_quote = data['Close'].iloc[-1]
+        print(ticker, last_quote)
+
+#
+# def get_current_price(symbol):
+#     ticker = yf.Ticker(symbol)
+#     todays_data = ticker.history(period='1d')
+#     return todays_data['Close'][0]
+
+
+if __name__ == "__main__":
+    signal.signal(signal.SIGINT, signal_handler)
+    print("Hello World")
+    schedule.every(3).minutes.until("16:30").do(job)
